e4a1ce76fdd69c1338951922a8423db1c25b8a78
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-12-04 09:22:01 +0100

    Created a requirements file.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Team project
+# project
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
     SECRET_KEY = '#######'  # Change this to a secure secret key
-    SQLALCHEMY_DATABASE_URI =  '####################'
+    SQLALCHEMY_DATABASE_URI =  'mysql://root:iyanu@localhost:3306/surveyapp'
+
 
