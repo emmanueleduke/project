035f2a61bfadf5eb63cdcd402f9773f6c280c7fb
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-12-10 23:46:30 +0100

    Reponse class

--- models/response.py
+++ models/response.py
@@ -1,12 +1,14 @@
-from models.storage.db_storage import Base
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey, Boolean, Integer
+from models.base import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Text
 from sqlalchemy.orm import relationship
 
-class Response(Base):
+class Response(BaseModel, Base):
     __tablename__ = 'response'
-    id = Column('id', Integer, primary_key=True, nullable=False)
-    users_id = Column('users_id', Integer, ForeignKey('users.id'), nullable=False)
-    survey_id = Column('survey_id', Integer, ForeignKey('survey.id'), nullable=False)
-    title = Column('title', String, nullable=False)
-    response = Column('response', String, nullable=False)
+    users_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    survey_id = Column(String(60), ForeignKey('survey.id'), nullable=False)
+    title = Column('title', String(24), nullable=False)
+    response = Column('response', Text, nullable=False)
+
+    def __init__(self, **kwargs):
+        """Create the instance"""
+        super().__init__(**kwargs)
