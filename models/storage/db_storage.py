b584539393ba270f76ad13611d84b438097f36a8
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-12-10 23:47:14 +0100

    database client file

--- models/storage/db_storage.py
+++ models/storage/db_storage.py
@@ -1,4 +1,7 @@
-import sqlalchemy
+from models.base import Base
+from models.user import User
+from models.survey import Survey
+from models.response import Response
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -6,12 +9,18 @@ from config import config
 from os import getenv
 
 
-DB_USER = config.get('db_user')
-DB_PWD = getenv('DB_PWD')
-DB_HOST = config.get('db_host')
-DB_PORT = config.get('DB_PORT')
+DB_USER = config.get('db_user') or 'root'
+DB_PWD = getenv('DB_PWD') or 'iyanu'
+DB_HOST = config.get('db_host') or '0.0.0.0'
+DB_PORT = config.get('db_port') or '3306'
 DB_NAME = config.get('db_name')
-Base = declarative_base()
+
+url = 'mysql+mysqldb://{}:{}@{}/{}'.format(DB_USER,
+                                  DB_PWD,
+                                  DB_HOST,
+                                  DB_NAME)
+
+classes = {'User': User, 'Survey': Survey, 'Response': Response}
 
 
 class Storage:
@@ -23,7 +32,9 @@ class Storage:
     def __init__(self):
         """Initialize the database
         """
-        self.__engine = create_engine(f'mysql+mysqldb//{DB_USER}:{DB_PWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+        self.__engine = create_engine(url)
+        Base.metadata.create_all(bind=self.__engine)
+
     def new(self, obj):
         """add the object to the current database session"""
         self.__session.add(obj)
