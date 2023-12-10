42322b1288b890c3eb6b5ffe0938a6866f5d6abf
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-12-10 23:43:10 +0100

    Survey class

--- models/survey.py
+++ models/survey.py
@@ -1,13 +1,15 @@
-from models.storage.db_storage import Base
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey, Boolean, Integer
+from models.base import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey, Text, Integer
 from sqlalchemy.orm import relationship
 
-class Survey(Base):
+class Survey(BaseModel, Base):
     __tablename__ = 'survey'
-    id = Column('id', Integer, primary_key=True, nullable=False)
-    creators_id = Column('creators_id', Integer, ForeignKey('users.id'), nullable=False)
-    title = Column('title', String, nullable=False)
-    description = Column('description', String, nullable=False)
-    form = Column('form', String, nullable=False)
-    response = relationship('response', backref='survey', cascade="all, delete, delete-orphan")
+    creators_id = Column('creators_id', String(60), ForeignKey('users.id'), nullable=False)
+    title = Column('title', String(60), nullable=False)
+    description = Column('description', String(256), nullable=False)
+    form = Column('form', Text, nullable=False)
+    # response = relationship('Response', back_populates='survey', cascade="all, delete, delete-orphan")
+
+    def __init__(self, **kwargs):
+        """Create the instance"""
+        super().__init__(**kwargs)
