06e13a78252d6ea71438dcfc5dfd7a3709efed06
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-12-11 19:08:35 +0100

    Config info

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-# class Config:
-#     SECRET_KEY = '#######'  # Change this to a secure secret key
-#     SQLALCHEMY_DATABASE_URI =  'mysql://root:iyanu@localhost:3306/surveyapp'
-
 config = {
     'db_user': 'root',
     'db_host': 'localhost',
