b84690f7beff5975864dc20a3806bb093be99092
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-12-21 14:20:48 +0100

    Script

--- web_app/static/scripts/thank_you.js
+++ web_app/static/scripts/thank_you.js
@@ -6,4 +6,19 @@ $(document).ready(() => {
     }, 1000);
     $("#create-survey").focus();
   });
+
+//   $('#copy1').click(() => {
+//       let copyText = $('#copyInput1').text();
+//       // copyText.select();
+//       console.log(copyText);
+//       document.execCommand("copy", copyText);
+//       alert("Text copied to clipboard: " + copyText);
+//   });
+
+//   $('#copy2').click(() => {
+//     let copyText = document.getElementById("copyInput2");
+//     copyText.select();
+//     document.execCommand("copy");
+//     alert("Text copied to clipboard: " + copyText.value);
+// });
 });
